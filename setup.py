4744dc398d599c9f61c43f38de54974431d9c0c8
Koray Gocmen <koray@gordiansoftware.com>
2023-07-10 19:48:35 +0300

    setup updates

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import find_packages, setup
 
-setup(
-    name="gconfig",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    author="Koray Gocmen",
-    author_email="koray@gordiansoftware.com",
-    description="Gordian global config library for Python",
-    license="MIT",
-    python_requires=">=2.7",
-)
+if __name__ == "__main__":
+    setup()
--- src/gconfig/__init__.py
+++ src/gconfig/__init__.py
@@ -1,4 +1,2 @@
-__version__ = "0.1.0"
-
 from .config import *
 from .exceptions import *
