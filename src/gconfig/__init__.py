cf448ebd05413eab5341d445e9dc8f4c1710f0a8
Jakub Klapacz <jakub@gordiansoftware.com>
2023-09-18 16:52:19 -0500

    aesthetic change

--- src/gconfig/__init__.py
+++ src/gconfig/__init__.py
@@ -1,26 +1,30 @@
 from .cache import Cache, CacheEntry
 from .config import Config
-from .exceptions import (AWSInvalidCredentialsException,
-                         AWSInvalidSessionException,
-                         AWSMissingAccessKeyIdException,
-                         AWSMissingRegionException, AWSMissingRoleARNException,
-                         AWSMissingRoleSessionNameException,
-                         AWSMissingSecretAccessKeyException,
-                         AWSMissingSessionTokenException)
+from .exceptions import (
+    AWSInvalidCredentialsException,
+    AWSInvalidSessionException,
+    AWSMissingAccessKeyIdException,
+    AWSMissingRegionException,
+    AWSMissingRoleARNException,
+    AWSMissingRoleSessionNameException,
+    AWSMissingSecretAccessKeyException,
+    AWSMissingSessionTokenException,
+)
 from .parse import parse_entry
 
-__version__ = "0.2.1"
 __all__ = [
-    'AWSInvalidCredentialsException',
-    'AWSInvalidSessionException',
-    'AWSMissingAccessKeyIdException',
-    'AWSMissingRegionException',
-    'AWSMissingRoleARNException',
-    'AWSMissingRoleSessionNameException',
-    'AWSMissingSecretAccessKeyException',
-    'AWSMissingSessionTokenException',
-    'Config',
-    'Cache',
-    'CacheEntry',
-    'parse_entry'
+    "AWSInvalidCredentialsException",
+    "AWSInvalidSessionException",
+    "AWSMissingAccessKeyIdException",
+    "AWSMissingRegionException",
+    "AWSMissingRoleARNException",
+    "AWSMissingRoleSessionNameException",
+    "AWSMissingSecretAccessKeyException",
+    "AWSMissingSessionTokenException",
+    "Cache",
+    "CacheEntry",
+    "Config",
+    "parse_entry",
 ]
+
+__version__ = "0.2.0"
